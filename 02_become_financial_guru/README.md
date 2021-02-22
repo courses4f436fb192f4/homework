5ec8b4cd005ca67cf4adbf51cd60d4213f830115
tux <tux@tux.tux>
2021-02-22 16:21:46 +0300

    02_task: added completed script

---
+++ 02_become_financial_guru/README.md
@@ -0,0 +1,29 @@
+## Geting started
+This script tell you which March the price was the least volatile since 2015. 
+Output show you the difference between MIN and MAX values for the period.
+## Instalation
+```sh
+   https://github.com/courses4f436fb192f4/homework.git
+   cd 01_turn_a_one-liner_into_a_nice_script/
+   chmod +x checkCons.sh
+```
+Download the database to folder with script
+```sh
+curl -s https://yandex.ru/news/quotes/graph_2000.json > ./quotes.json
+```
+
+## Requirements
+* Linux OS
+* bash
+* curl
+* qj
+
+## Usage
+You can use script without any argumets or
+send to him a month "03". "03" is by default. 
+
+## Changelog
+
+20.02.2021
+* creation date
+
--- 02_become_financial_guru/finance.sh
+++ 02_become_financial_guru/finance.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
-MIN_VALUE="100000"
-MAX_VALUE="0"
+WARING_COLOR="\033[0;33m"
+NO_COLOR="\033[0m"
+
+ARG_MONTH="03"
 
 DATE_LIST=""
 JQ_VALUES=""
 NEW_JQ_VALUES=""
 TMP_VALUE=""
-
+RESULTS_VALUE=""
 
 JQ_VALUES=$(jq -r '.prices[][]' quotes.json)
 
 while read line_date; read line_value;
 do
     TMP_VALUE="$( echo $line_date | sed 's/...$//' )"
-    NEW_JQ_VALUES+="$( date -d @"$TMP_VALUE" "+%m-%y" ) $line_value \n"
+    NEW_JQ_VALUES+="$( date -d @"$TMP_VALUE" "+%m-%Y" ) $line_value \n"
 done <<< "$JQ_VALUES" 
 
+if [ -n "$1" ]
+then
+    ARG_MONTH="$1"
+fi
+
 ### !!! short list of values
-NEW_JQ_VALUES="$( echo -e "$NEW_JQ_VALUES" | grep ^03 )"
-DATE_LIST="$( echo "$NEW_JQ_VALUES" | grep ^03 | cut -d " " -f1 | uniq )"
+NEW_JQ_VALUES="$( echo -e "$NEW_JQ_VALUES" | grep ^"$ARG_MONTH" )"
+DATE_LIST="$( echo "$NEW_JQ_VALUES" | grep ^"$ARG_MONTH" | cut -d " " -f1 | uniq )"
 
 while read date_value;
 do
-    echo "================================="
-    echo "$date_value"
-    echo "======"
-    TMP_VALUE="$(echo "$NEW_JQ_VALUES" | grep "$date_value" | cut -d " " -f2 )"
-#    echo "$TMP_VALUE"
 
+    TMP_VALUE="$(echo "$NEW_JQ_VALUES" | grep "$date_value" | cut -d " " -f2 )"
+
+    RESULTS_VALUE+="$date_value "
     while read line;    
     do
-    awk -v min_val=$MIN_VALUE -v max_val=$MAX_VALUE '{ cur_val=$1;
+    RESULTS_VALUE+="$(awk -v min_val=1000 -v max_val=0 '{ cur_val=$1;
             if (min_val > cur_val){
                 min_val=cur_val
             }
@@ -40,16 +45,27 @@ do
             }
         }
         END {
-            print "min_val is: " min_val
-                print "max_val is: " max_val
-            }'
-#    echo "$line"
-#    echo "$MIN_VALUE"
-    echo "loop ****************************"
-    echo
-    done <<< "$TMP_VALUE"
-    
-    
+                print min_val " " max_val " " (max_val-min_val)/2
+            }')\n"
+    done <<< "$TMP_VALUE"    
 done <<< "$DATE_LIST"
 
-#echo "$NEW_JQ_VALUES"
+RESULTS_VALUE=$( echo -e "$RESULTS_VALUE" | sort "-nk 4" | sed '/^$/d')
+
+echo
+echo "The least volatile in: "
+echo -en "$WARING_COLOR"
+echo -en "$RESULTS_VALUE" | head -n 1 | cut -d " " -f1
+echo -en "$NO_COLOR"
+echo "and equls:"
+echo -en "$WARING_COLOR"
+echo -e "$RESULTS_VALUE" | head -n 1 | cut -d " " -f4
+echo -e "$NO_COLOR"
+
+echo -e "$RESULTS_VALUE" | awk 'BEGIN{
+    print "date     min     max     volatile"
+    }
+    {
+    printf "%-8s %3.4f %3.4f %3.4f\n", $1, $2, $3, $4
+}'
+echo
