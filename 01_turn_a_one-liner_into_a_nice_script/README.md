096aa1d6b7e73956a0f46ee7ce0068ec96365d7c
tux <tux@tux.tux>
2021-02-27 00:24:50 +0300

    01_task: fixed some mistakes

--- 01_turn_a_one-liner_into_a_nice_script/README.md
+++ 01_turn_a_one-liner_into_a_nice_script/README.md
@@ -17,15 +17,4 @@ PID or process name -> netstat -> whois.
 ## Usage
 To normaly use this script you need to escalate privileges.
 Run script as sudo with input argument which contains
-part of process name or pid. An optional parameter is a filter
-such as "^Organization" which is the default parameter
-
-## Changelog
-
-19.02.2021
-* added check for the existence of a process
-* added the ability to apply filters
-
-16.02.2021
-* creation date
-* basic functionality
+part of process name or pid.
--- 01_turn_a_one-liner_into_a_nice_script/checkCons.sh
+++ 01_turn_a_one-liner_into_a_nice_script/checkCons.sh
@@ -4,12 +4,12 @@ WARING_COLOR="\033[0;33m"
 ERR_COLOR="\033[0;31m"
 NO_COLOR="\033[0m"
 
-
 INPUT_PROCESS="$1"
-INPUT_FIELD_TO_VIEW="$2"
+INPUT_FIELD_TO_VIEW=""
 TMP_VALUE=""
 OPERATION_EXIT_CODE=""
-
+CONN_LINE=""
+CONN_LINE_TMP=""
 
 f_check_if_process_exist()
 {
@@ -18,7 +18,7 @@ f_check_if_process_exist()
             echo "PID"
             if [ -d "/proc/$INPUT_PROCESS" ]  
             then
-                netstat -tunapl | grep $INPUT_PROCESS
+                netstat -tunapl | grep $INPUT_PROCESS > /dev/null
                 OPERATION_EXIT_CODE=$?
             else
                 echo "doesnt exists"
@@ -36,7 +36,7 @@ f_check_if_arg_2_exist()
 {
     if [ -z "$INPUT_FIELD_TO_VIEW" ]
     then
-        INPUT_FIELD_TO_VIEW="^Organization"
+        INPUT_FIELD_TO_VIEW="Organization"
     fi
 }
 
@@ -51,16 +51,25 @@ then
 
         if [ "$OPERATION_EXIT_CODE" -eq 0 ]
         then
-            echo -e  "$WARING_COLOR using netstat $NO_COLOR"
+            echo -e  "$WARING_COLOR USING netstat $NO_COLOR"
 
             TMP_VALUE=`netstat -tunapl`
             TMP_VALUE=$(awk "/$INPUT_PROCESS/ {print \$5}" <<< "$TMP_VALUE")
-            TMP_VALUE=$( echo -e "$TMP_VALUE" | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+' )
-            
+            TMP_VALUE=`echo -e "$TMP_VALUE" | grep -oP '(\d+\.){3}\d+' | uniq`
+
+            echo -e "$WARING_COLOR IN_PROGRESS:";
             while read IP
             do
-                whois "$IP" | awk -F":" "/$INPUT_FIELD_TO_VIEW/ {print \$2}"
+            echo -e -n "*"
+                CONN_LINE_TMP=`whois "$IP" | awk -F":" "/"^"$INPUT_FIELD_TO_VIEW/ {print \$2}"`
+                if [ -n "$CONN_LINE_TMP" ]
+                then
+                    CONN_LINE+="$CONN_LINE_TMP\n"
+                else
+                    CONN_LINE+="$INPUT_FIELD_TO_VIEW: $IP\n"
+                fi
             done <<< "$TMP_VALUE" 
+            echo -e "$NO_COLOR"
         else
             echo -e "$WARING_COLOR Process doesn't have any connections $NO_COLOR"
         fi
@@ -68,3 +77,8 @@ then
 else
     echo -e "$WARING_COLOR No args $NO_COLOR" 
 fi
+    
+while read line
+do
+    echo "Connections count: $line"
+done <<< "$( echo -e "$CONN_LINE" | sort | uniq -c | sed 's/Organization://g' )"
